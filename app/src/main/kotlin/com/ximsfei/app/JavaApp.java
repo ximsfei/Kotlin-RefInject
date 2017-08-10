e281801db0642965630c957ac09304d8193de963
pengfengwang <pengfengwang@sohu-inc.com>
2017-08-10 17:29:50 +0800

    Java example

--- app/src/main/kotlin/com/ximsfei/app/JavaApp.java
+++ app/src/main/kotlin/com/ximsfei/app/JavaApp.java
@@ -8,9 +8,12 @@ public class JavaApp {
         ActivityThread origin = new ActivityThread();
         System.out.println(ActivityThread.class);
         System.out.println(origin);
-//        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getMApplication().set(origin, "Inject Application");
-//        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getMInstrumentation().set(origin, "Inject Instrumentation");
-//        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getMBoolean().set(origin, false);
+        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getMApplication().set(origin, "Inject Application");
+        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getMInstrumentation().set(origin, "Inject Instrumentation");
+        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getMBoolean().set(origin, false);
+        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getGetVal();
+        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getPrintAAndB().call(origin, 1, 2);
+        mirror.com.ximsfei.app.ActivityThread.INSTANCE.getPrintAAndBRef().call(origin, 2, 3);
         System.out.println(mirror.com.ximsfei.app.ActivityThread.INSTANCE.getTYPE());
         System.out.println(origin);
     }
